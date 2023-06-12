7bc318fa20ea33290753f20720b9bd7527378a63
xfnw <xfnw@riseup.net>
2023-06-12 14:44:03 -0400

    implement parsc

--- parsc.c
+++ parsc.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define stok(varname, ptr, delim) varname = strtok(ptr, delim); \
+	if (varname == NULL) return
+
+void doline(char *line) {
+	char *name, *cmd;
+
+	stok(name, line, " :\n");
+
+	do {
+		stok(cmd, NULL, " \n");
+	} while (*cmd>>6 != 1); /* go until letter */
+
+	if (!strcmp("KILL", cmd)) {
+		/* use name from KILL parameter */
+		stok(name, NULL, " :\n");
+	}
+
+	printf("%s %s\n", cmd, name);
+}
+
+int main() {
+	char *linebuf = malloc(1024);
+	ssize_t linesize = 1024;
+
+	while (getline(&linebuf, &linesize, stdin) != -1) {
+		char *line = linebuf;
+		doline(line);
+	}
+
+	free(linebuf);
+}
 
