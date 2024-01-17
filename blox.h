084fac1d9d25d587dad5688522919f0fa6fba10d
xfnw <xfnw@riseup.net>
2024-01-17 10:06:19 -0500

    visyear: create
    visualize a year like a github-style contibution graph

---
+++ blox.h
@@ -0,0 +1,38 @@
+/* SPDX-FileCopyrightText: 2024 xfnw
+ * SPDX-License-Identifier: MIT
+ *
+ * teeny tiny unicode block character drawing library
+ * named after a very nice feesh :3
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+
+const char *blox_disp_chars[] = {" ", "▀", "▄", "█"};
+
+void blox_draw(unsigned char *frame) {
+	unsigned short fx, fy;
+	for (fy = 0; fy < BLOX_RH; fy++) {
+		for (fx = 0; fx < BLOX_RW; fx++) {
+			unsigned char s, c;
+			c = frame[BLOX_RW * fy + fx];
+			for (s = 0; s < 4; s++) {
+				fputs(blox_disp_chars[c & 3], stdout);
+				c >>= 2;
+			}
+		}
+		putc('\n', stdout);
+	}
+}
+
+void blox_set(unsigned char *frame, unsigned short x, unsigned short y) {
+	frame[BLOX_RW * (y >> 1) + x / 4] |= 1 << ((x % 4) * 2 + (y & 1));
+}
+
+void blox_del(unsigned char *frame, unsigned short x, unsigned short y) {
+	frame[BLOX_RW * (y >> 1) + x / 4] &= ~(1 << ((x % 4) * 2 + (y & 1)));
+}
+
+bool blox_get(unsigned char *frame, unsigned short x, unsigned short y) {
+	return frame[BLOX_RW * (y >> 1) + x / 4] & 1 << ((x % 4) * 2 + (y & 1));
+}
---
+++ visyear.c
@@ -0,0 +1,54 @@
+/* SPDX-FileCopyrightText: 2024 xfnw
+ * SPDX-License-Identifier: MIT
+ *
+ * visualize a year in the style of github's contribution charts
+ *
+ * takes ISO 8601-like dates at the start of stdin lines and shows
+ * the previous year. everything after the first space is ignored.
+ */
+
+#define _GNU_SOURCE
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define W 64
+#define H 8
+
+#define BLOX_RW W / 4
+#define BLOX_RH H / 2
+
+#include "blox.h"
+
+int main() {
+	unsigned char frame[BLOX_RW * BLOX_RH] = { 0 };
+	time_t now = time(NULL);
+	struct tm *t = localtime(&now);
+	int days = 364 + t->tm_wday;
+
+	/* avoid segfaulting on sundays */
+	if (days < 365)
+		days += 7;
+
+	char *line = NULL;
+	size_t size;
+
+	while (-1 != getline(&line, &size, stdin)) {
+		char *fst = strtok(line, " \r\n");
+
+		memset(t, 0, sizeof(t));
+		strptime(fst, "%Y-%m-%d", t);
+		int ago = (int)((now - mktime(t)) / 86400);
+
+		if (ago >= 365 || ago < 0)
+			continue;
+
+		blox_set(frame, (days - ago) / 7, (days - ago) % 7);
+	}
+
+	free(line);
+	blox_draw(frame);
+}
